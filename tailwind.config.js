84a5a46d653410e51cacb7efab454f7b55da0fa8
Alysander Stanley <alysander@gmail.com>
2021-08-09 00:04:59 +1000

    Changed spacing scale and tried it on the root div

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    spacing: {
+      1: "10px",
+      2: "20px",
+      3: "30px",
+      4: "40px",
+      5: "50px",
+      6: "60px",
+    },
   },
   variants: {
     extend: {},
