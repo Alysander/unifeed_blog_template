e23c817bb6faef2ce594a63bd859244f927b4c99
Alysander Stanley <alysander@gmail.com>
2021-08-09 10:23:40 +1000

    Imported logo svg and tweaked root container spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import logo from './assets/Logo.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="container px-2">
-
+    <div className="container px-2 py-4 ">
+      <img src={logo} alt="Uni blog logo" />
     </div>
   );
 }
