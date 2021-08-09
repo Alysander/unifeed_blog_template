826a17870e0b3a925ee1a490876089d470effac9
Alysander Stanley <alysander@gmail.com>
2021-08-09 10:47:37 +1000

    Allow children in LogoBar

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import logo from '../assets/Logo.svg'
 
 
-const LogoBar = () => (
-    <img src={logo} alt="Unifeed logo" />
+const LogoBar = ({ children }: { children?: React.ReactNode }) => (
+
+    <div>
+        <img src={logo} alt="Unifeed logo" />
+        {children}
+    </div>
+
 )
 
 
