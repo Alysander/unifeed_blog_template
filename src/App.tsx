ca271c4673d864563d3798a713d881aa8b1441f8
Alysander Stanley <alysander@gmail.com>
2021-08-09 10:52:38 +1000

    Add hamburger menu icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import { LogoBar } from './components/LogoBar';
+import { hamburgerMenu } from './assets/HamburgerMenu.svg';
 
 function App() {
   return (
     <div className="container px-2 py-4 ">
-      <LogoBar />
+      <LogoBar>
+        <img src={hamburgerMenu} alt="Menu" />
+      </LogoBar>
     </div>
   );
 }
