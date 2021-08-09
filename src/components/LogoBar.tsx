6fc2014e87a2d8ecb954d20677e35698cdbbbc6b
Alysander Stanley <alysander@gmail.com>
2021-08-10 09:56:58 +1000

    Add vertical margin for header and footer for md

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -3,7 +3,7 @@ import { UnifeedLogo } from './UnifeedLogo';
 
 const LogoBar = ({ children }: { children?: React.ReactNode }) => (
 
-    <div className="flex">
+    <div className="flex my-4 md:my-7">
         <UnifeedLogo />
         <div className="flex-1" />
         <div className="flex-initial">
