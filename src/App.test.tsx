dddc838231b0f2ac647966e40dfc222413cc3bd9
Alysander Stanley <alysander@gmail.com>
2021-08-12 02:00:29 +1000

    Add to 'opens menu' test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -24,12 +24,18 @@ describe("the hamburger menu", () => {
 
   test('renders the hamburger menu', async () => {
     render(<App />);
-    expect(await screen.findByAltText(/Menu/)).toBeInTheDocument();
+    expect(await screen.findByAltText(/Menu button/)).toBeInTheDocument();
   })
-  test('clicking the menu button opens the menu', () => {
+  test('clicking the menu button opens the menu', async () => {
     render(<App />);
     const menu = screen.getByAltText(/Menu/);
 
     expect(menu).toBeInTheDocument()
+    menu.click();
+
+    expect(await screen.findByText("Home")).toBeInTheDocument();
+    expect(await screen.findByText("Features")).toBeInTheDocument();
+    expect(await screen.findByText("Pricing")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Subscribe" })).toBeInTheDocument();
   })
 })
