ad2b33752f8afc16eebc9af353143b610a28fa6b
Alysander Stanley <alysander@gmail.com>
2021-08-13 16:12:06 +1000

    Horizontal nav list for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 import { UnifeedLogo } from './UnifeedLogo';
 import hamburgerMenu from '../assets/HamburgerMenu.svg';
 
@@ -11,36 +11,36 @@ const Header = () => {
         <header className="my-4 md:my-7">
             <div className="flex justify-between mb-2">
                 <UnifeedLogo />
-                <button>
+                <button className="md:hidden">
                     <img src={hamburgerMenu} alt="Menu" onClick={() => { setMobileMenuOpen(!mobileMenuOpen) }} />
                 </button>
+                <NavList className="hidden md:block" />
+
             </div>
-            <nav className={"text-l text-mediumGray transform transition duration-200 ease-in " + (
-                mobileMenuOpen ? "opacity-100 transform scale-y-1 origin-top h-auto"
-                    : "opacity-0 scale-y-0 origin-top  h-1px ")}>
-                <ul className="flex flex-col gap-1 bg-white">
-                    <a href="#">
-                        <li>Home</li>
-                    </a>
-                    <a href="#">
-                        <li>Features</li>
-                    </a>
-                    <a href="#">
-                        <li>Pricing</li>
-                    </a>
-                    <a>
-                        <button className="rounded-lg bg-orange text-black font-semibold p-1">
-                            Subscribe
-                        </button>
-                    </a>
-
-                </ul>
-            </nav>
-
+            <NavList className={"md:hidden transform transition  ease-in duration-200 origin-top " + (
+                mobileMenuOpen ? "opacity-100 scale-y-100 h-auto"
+                    : "opacity-0 scale-y-0 h-1px ")} />
         </header>
-
     )
 }
 
-
+const NavList = ({ className }: { className: string }) => (
+    <nav className={className}>
+        <ul className="flex flex-col gap-2 bg-white md:flex-row text-l text-mediumGray">
+            <ListLink>Home</ListLink>
+            <ListLink>Features</ListLink>
+            <ListLink>Pricing</ListLink>
+            <ListLink>
+                <button className="rounded-lg bg-orange text-black font-semibold px-2 py-1">
+                    Subscribe
+                </button>
+            </ListLink>
+        </ul>
+    </nav >
+)
+const ListLink = ({ children }: { children: ReactNode }) => (
+    <a href="#" className="self-center">
+        <li>{children}</li>
+    </a>
+)
 export { Header }
